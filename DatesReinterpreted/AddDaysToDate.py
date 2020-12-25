5ce739f7058bade4b8bf885038aa2c2af3b04b44
AdamOIC <63672393+AdamOIC@users.noreply.github.com>
2020-12-25 11:21:10 -0500

    Update AddDaysToDate.py
    Accept user input for the start date and days to add, and validate the string input.
    For dates, first check with a regex before passing to datetime and catching ValueErrors

--- DatesReinterpreted/AddDaysToDate.py
+++ DatesReinterpreted/AddDaysToDate.py
@@ -13,34 +13,94 @@ it pushes it out to Monday.
 
 '''
 
-from datetime import date, timedelta
+import datetime as dt
+import re
+
+#Functions
+def datestr2dateobj(date_str, delim):
+    '''Take a string formatted date and turn it into a datetime object
+        The string should be mm dd yyyy, with delim identifying - or /'''
+    str_format = "%m/%d/%Y"
+    if delim == "-":
+        str_format = "%m-%d-%Y"
+
+    try:
+        return dt.datetime.strptime(date_str, str_format)
+
+    except(ValueError):
+        print("The date entered is not correct, please try again")
+        return False
+
+
+def identify_date_str(date_str):
+    '''#function to figure out what date format they entered mm/dd/yyyy or
+     mm-dd-yyyy '''
+    fs, ds = "/", "-"
+
+    #narrow it down by the delimiter user entered then match a pattern
+    if fs in date_str:
+        #mm/dd/yyyy
+        pattern = r'\d{1,2}/\d{1,2}/\d{4}'
+        res = re.search(pattern, date_str)
+        if res is not None:
+            return datestr2dateobj(date_str, fs)
+
+    elif ds in date_str:
+        #mm-dd-yyyy
+        pattern = r'\d{1,2}-\d{1,2}-\d{4}'
+        res = re.search(pattern, date_str)
+        if res is not None:
+            return datestr2dateobj(date_str, ds)
+
+    else:
+        #no match
+        print("Enter the date as a month, day, and four digit year using a ")
+        print("dash '-' or forward slash '/'")
+        return False
+
+
+#End Functions
+
 
 start_date = None
-#start_date = input("Enter starting date as mm/dd/yyyy or leave blank for today: "))
-if start_date is None:
-    start_date = date.today()
 
-add_days= int(input("Enter number of weekdays to add: "))
+#To default to today
+#if start_date is None:
+#   start_date = dt.date.today()
 
+#For user entered and code validated date
+while start_date is None or start_date is False:
+    start_date = identify_date_str(input("Enter starting date as mm/dd/yyyy or mm-dd-yyyy: "))
+
+#Number of Days to Add to a valid date
+add_days = None
+while add_days is None:
+    try:
+        add_days = int(input("Enter number of weekdays to add: "))
+    except(ValueError):
+        print("\nEnter a whole number (ex. 5, 69)")
+        add_days = None
+
+#Process return value
 if add_days <= 5:
     if (start_date.weekday() + add_days) < 5: 
         #the number of days being added will be within the same business week
-        end_date = start_date + timedelta(days = add_days)
+        end_date = start_date + dt.timedelta(days = add_days)
     else:
         #it would end on a Saturday or goes through the full weekend
-        end_date = start_date + timedelta(days = (add_days + 2))
+        end_date = start_date + dt.timedelta(days = (add_days + 2))
 
 else:
     #This adds on for larger amounts of days, by considering the passing of
-    # "business" weeks
+    # "business" weeks by Charles
     calendarDaysToAdd = (add_days // 5 * 7) + (add_days % 5)
-    print("Redmond's: Add", add_days,"to", start_date, "(",calendarDaysToAdd,")")
+    print("Add", add_days,"to", start_date, "(",calendarDaysToAdd,")")
 
-    end_date = start_date + timedelta(days = calendarDaysToAdd)
+    end_date = start_date + dt.timedelta(days = calendarDaysToAdd)
     print(end_date)
     if (end_date.weekday() == 5):
-        end_date = start_date + timedelta(days = calendarDaysToAdd + 2)
+        end_date = start_date + dt.timedelta(days = calendarDaysToAdd + 2)
     elif (end_date.weekday() == 6):
-        end_date = start_date + timedelta(days = calendarDaysToAdd + 1)
+        end_date = start_date + dt.timedelta(days = calendarDaysToAdd + 1)
 
 print("End Date:",end_date)
